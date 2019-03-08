38fd4c6a5f50d9b5a8ec79e3f88183fe6ec58123
Rowan Smith <theintheredwoods@gmail.com>
2019-03-08 16:04:28 -0600

    completed addEmployee function

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,7 @@
 $(document).ready(readyNow)
 
+let employees = []
+
 function readyNow(){
     console.log( 'ready now' );
     $('#submitButton').on('click', addEmployee)
@@ -13,8 +15,19 @@ function addEmployee(){
         jobTitle: $( '#jobTitle' ).val(),
         annualSalary: $( '#annualSalary' ).val(),
         delete: ''
-    }//end addEmployee
-}
+    }//end newEmployee
+    employees.push( newEmployee );
+    //empty input
+    $( '#firstName' ).val(''),
+    $( '#lastName' ).val(''),
+    $( '#employeeID' ).val(''),
+    $( '#jobTitle' ).val(''),
+    $( '#annualSalary' ).val(''),
+    console.log( 'employees', employees)
+    // displayEmployees();
+}//end addEmployee
+
+
 //Add an employee
 function addEmployee(){
     new Employee(``)
